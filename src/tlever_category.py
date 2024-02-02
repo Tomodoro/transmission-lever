a87a9941f1d1d58aa474222268d1dfeccceb3858
Tomodoro Kita <tomodoro@salcyeka.com>
2024-02-02 14:16:20 -0300

    refactor: delegate client session to module

--- src/tlever_category.py
+++ src/tlever_category.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 import os
-from transmission_rpc import Client
 from tlever_label import mk_label, rm_label
 from tlever_torrent import mv_data
-from tlever_client import get_downloads_dir
+from tlever_client import get_downloads_dir, get_client
 
 
 def category_prefix(config) -> str:
@@ -18,20 +17,20 @@ def category_prefix(config) -> str:
     return config['General']['prefix']['categories']
 
 
-def mk_category(client: Client,
-                config: dict,
+def mk_category(config: dict,
                 torrent_hash: str,
                 category_name: str
                 ) -> None:
 
     """
     Create an emulated category through labels
-    :param client: valid transmission session
     :param config: valid configuration dictionary
     :param torrent_hash: hash of a single torrent
     :param category_name: name of the category
     :return: None
     """
+
+    client = get_client(config)
 
     label = category_prefix(config) + category_name
     mk_label(client, torrent_hash, label)
@@ -42,20 +41,20 @@ def mk_category(client: Client,
     return
 
 
-def rm_category(client: Client,
-                config: dict,
+def rm_category(config: dict,
                 torrent_hash: str,
                 category_name: str
                 ) -> None:
 
     """
     Delete an emulated category through labels
-    :param client: valid transmission session
     :param config: valid configuration dictionary
     :param torrent_hash: hash of a single torrent
     :param category_name: name of the category
     :return: None
     """
+
+    client = get_client(config)
 
     directory = get_downloads_dir(client)
     mv_data(client, torrent_hash, directory)
