3261dbff053d9e6c8074d300e9eb11907fb4fb68
Tomodoro Kita <tomodoro@salcyeka.com>
2024-02-02 09:41:17 -0300

    feat: add cli

--- src/tlever.py
+++ src/tlever.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+import os
+import sys
 import json
+import logging
+import argparse
 from transmission_rpc import Client
+from tlever_category import mk_category, rm_category, downloads_path
+from tlever_label import mk_label, rm_label
+from tlever_tag import mk_tag, rm_tag
 
-f = open('../transmission-lever.json')
+logging.basicConfig(level=logging.INFO)
+
+f = open(os.path.dirname(__file__) + "/../transmission-lever.json")
 
 cfg = json.load(f)
 
@@ -12,9 +21,91 @@ client = Client(host=cfg["Client"]["host"],
                 username=cfg["Client"]["username"],
                 password=cfg["Client"]["password"])
 
-print(client.server_version)
+#id = "191d8c692bd1e58339093a96a2111d5370f065e8"
+#torrent = client.get_torrent(torrent_id=id)
+#print(torrent)
+#sys.exit()
 
-torrents = client.get_torrents()
+# create the top level parser
+parser = argparse.ArgumentParser(
+    description='simplifies common chores on torrents by wrapping the RPC',
+    epilog='not all RPC methods are properly documented on upstream')
 
-for torrent in torrents:
-    print(torrent)
+subparsers = parser.add_subparsers(help='modifier on a torrent',
+                                   dest='subparser_name',
+                                   required=True)
+
+# create the parser for the "category" command
+subparser_category = subparsers.add_parser('category',
+                                           help='manages categories of torrents')
+
+subparser_category.add_argument('action',
+                                type=str,
+                                choices=['add', 'remove'],
+                                help='action to perform')
+
+subparser_category.add_argument('name',
+                                type=str,
+                                help='the name of the category')
+subparser_category.add_argument('hash',
+                                type=str,
+                                help='the hash of the torrent')
+
+# create the parser for the "label" command
+subparser_label = subparsers.add_parser('label',
+                                        help='manages labels of torrents')
+
+subparser_label.add_argument('action',
+                             type=str,
+                             choices=['add', 'remove'],
+                             help='action to perform')
+
+subparser_label.add_argument('name',
+                             type=str,
+                             help='the name of the label')
+subparser_label.add_argument('hash',
+                             type=str,
+                             help='the hash of the torrent')
+
+# create the parser for the "label" command
+subparser_label = subparsers.add_parser('tag',
+                                        help='manages tags of torrents')
+
+subparser_label.add_argument('action',
+                             type=str,
+                             choices=['add', 'remove'],
+                             help='action to perform')
+
+subparser_label.add_argument('name',
+                             type=str,
+                             help='the name of the tag')
+subparser_label.add_argument('hash',
+                             type=str,
+                             help='the hash of the torrent')
+
+# parse arguments
+args = parser.parse_args()
+
+print(vars(args))
+
+if args.subparser_name == 'category':
+    if args.action == 'add':
+        mk_category(client, args.hash, args.name)
+
+    elif args.action == 'remove':
+        rm_category(client, args.hash, args.name)
+
+elif args.subparser_name == 'label':
+    if args.action == 'add':
+        mk_label(client, args.hash, args.name)
+
+    elif args.action == 'remove':
+        rm_label(client, args.hash, args.name)
+
+elif args.subparser_name == 'tag':
+    if args.action == 'add':
+        mk_tag(client, args.hash, args.name)
+
+    elif args.action == 'remove':
+        rm_tag(client, args.hash, args.name)
+
